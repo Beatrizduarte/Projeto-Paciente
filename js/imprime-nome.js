e90dfc2c534272639eb266a8b40e554003c6a942
Beatriz Duarte <beatriznduarte1@gmail.com>
2017-08-16 17:14:36 -0300

    Implementação de javascript para funcionamento do site e dos botoes do layout

--- js/imprime-nome.js
+++ js/imprime-nome.js
@@ -1,20 +1,14 @@
-
-var botao = document.getElementById("Calcular-imc");
-botao.addEventListener("click",function(){
-
 var trsPacientes = document.getElementsByClassName("paciente");
 
 percorreArray(trsPacientes, function(pacienteTr){
-
-
 	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
 	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
 	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-	var pacienteAtual = {nome: tdNome.textContent, 
-					peso: tdPeso.textContent, 
-					altura: tdAltura.textContent,
-					pegaImc: function(){
+	var pacienteAtual = {nome: tdNome.textContent,
+						peso: tdPeso.textContent,
+						altura: tdAltura.textContent,
+						pegaImc: function(){
 							if(this.altura != 0){
 								var imc = this.peso / (this.altura * this.altura);
 								return imc;
@@ -24,12 +18,5 @@ percorreArray(trsPacientes, function(pacienteTr){
 
 						}};
 
-	var imc = pacienteAtual.pegaImc();
-
-		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-		tdImc.textContent = imc;
-
-		console.log(imc);
-})
-
+	console.log(pacienteAtual.nome);
 });
